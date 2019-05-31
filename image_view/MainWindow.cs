a41d8554cd282e7bc20b7cd177808ede18381a1d
Michał Piechura <piechureq@wp.pl>
2019-05-31 20:20:39 +0200

    basic

---
+++ image_view/MainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Gtk;
+using image_view;
+
+public partial class MainWindow : Gtk.Window
+{
+    // protected Gtk.FileChooserWidget FileChooserWidget;
+    // protected Gtk.Image ImageView;
+    // protected Gtk.Button PrevBtn;
+    // protected Gtk.Button NextBtn;
+
+    private PicturesPool picturesPool = new PicturesPool();
+
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        OnShownSetup();
+    }
+
+    protected void OnShownSetup()
+    {
+        // set up 
+        FileFilter filter = new FileFilter();
+        filter.Name = "PNG and JPEG images";
+        filter.AddMimeType("image/png");
+        filter.AddPattern("*.png");
+        filter.AddMimeType("image/jpeg");
+        filter.AddPattern("*.jpg");
+        this.FileChooserWidget.Filter = filter;
+        Console.WriteLine("setup");
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void onFolderChanged(object sender, EventArgs e)
+    {
+    }
+
+    protected void onFileSelected(object sender, EventArgs e)
+    {
+        // get all in folder
+        picturesPool.SetPool(this.FileChooserWidget.Filename);
+
+        this.ImageView.File = this.FileChooserWidget.Filename;
+    }
+}
---
+++ image_view/PicturesPool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace image_view
+{
+    public class PicturesPool
+    {
+        private List<string> pool = new List<string>();
+        private Int16 index;
+
+        public PicturesPool()
+        {
+
+        }
+
+        public bool SetPool(String name)
+        {
+            if (String.IsNullOrWhiteSpace(name) == false)
+            {
+                pool.Add(name);
+            }
+            return false;
+        }
+
+        public String GetNext()
+        {
+            Console.WriteLine("x");
+            return "";
+        }
+
+        public String GetPrev()
+        {
+            Console.WriteLine("x");
+            return "";
+        }
+    }
+}
